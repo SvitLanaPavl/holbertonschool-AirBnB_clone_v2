6a1375f632661fb203fdaae71e68f4475f052f35
Svitlana PAvlovska <lanapavlovska90@gmail.com>
2023-11-10 11:42:28 -0800

    I have written the code but am unable to test because of the bugs unrelated to this particular task

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+'''Flask module documentation'''
+from flask import Flask, render_template
+from markupsafe import escape
+from models import storage
+
+
+app = Flask(__name__)
+
+
+@app.route('/states_list', strict_slashes=False)
+def display_html():
+    '''Displays the html page'''
+    states = storage.all('State')
+    return render_template('7-states_list.html', list=states)
+
+
+@app.teardown_appcontext
+def close():
+    '''teardown closing storage'''
+    storage.close()
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
