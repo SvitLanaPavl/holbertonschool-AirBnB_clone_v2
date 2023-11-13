127d2da0df9cafc3cea832f1c84ad20fe5bbe9e4
Svitlana PAvlovska <lanapavlovska90@gmail.com>
2023-11-13 11:54:42 -0800

    anyway.... tasks written unable to test due to weird error

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 '''Flask module documentation'''
 from flask import Flask, render_template
-from models import storage, State
+from models import storage, State, City
 
 
 app = Flask(__name__)
@@ -13,11 +13,11 @@ def close():
     storage.close()
 
 
-@app.route('/states_list', strict_slashes=False)
+@app.route('/cities_by_states', strict_slashes=False)
 def display_html():
     '''Displays the html page'''
     states = storage.all('State')
-    return render_template('7-states_list.html', states=states)
+    return render_template('8-cities_by_states.html', states=states)
 
 
 if __name__ == '__main__':
