11f7c0a162207a56d27e38813a7de74629cb865c
Svitlana PAvlovska <lanapavlovska90@gmail.com>
2023-11-13 15:03:49 -0800

    interm success on task 10

---
+++ web_flask/9-states.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+'''Flask module documentation for State City
+listens to host='0.0.0.0', port=5000
+renders the html page
+'''
+from flask import Flask, render_template
+from models import storage
+
+
+app = Flask(__name__)
+
+
+@app.route('/states', strict_slashes=False)
+def display_html():
+    '''Displays the html page'''
+    states = storage.all('State').values()
+    return render_template('9-states.html', states=states)
+
+
+@app.route('/states/<id>', strict_slashes=False)
+def display_html_id(id):
+    '''Display the html with id'''
+    states = storage.all('State').values()
+    for state in states:
+        if state.id == id:
+            return render_template('9-states', state=state)
+    return render_template('9-states')
+
+
+@app.teardown_appcontext
+def close(exc):
+    '''teardown closing storage'''
+    storage.close()
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
