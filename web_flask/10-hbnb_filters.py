1633808422005d79e0f4160843929c94f2e7748a
Svitlana PAvlovska <lanapavlovska90@gmail.com>
2023-11-14 21:05:57 -0800

    some cleanup and minor adjustments in file 10

--- web_flask/10-hbnb_filters.py
+++ web_flask/10-hbnb_filters.py
@@ -4,9 +4,6 @@ Listens to host='0.0.0.0', port=5000
 renders the html page'''
 from flask import Flask, render_template
 from models import storage
-from models.city import City
-from models.state import State
-from models.amenity import Amenity
 
 
 app = Flask(__name__)
@@ -15,11 +12,10 @@ app = Flask(__name__)
 @app.route('/hbnb_filters', strict_slashes=False)
 def display_html_filters():
     '''render html content'''
-    states = storage.all(State).values()
-    cities = storage.all(City).values()
-    amenities = storage.all(Amenity).values()
+    states = storage.all('State').values()
+    amenities = storage.all('Amenity').values()
     return render_template('10-hbnb_filters.html', states=states,
-                           cities=cities, amenities=amenities)
+                           amenities=amenities)
 
 
 @app.teardown_appcontext
